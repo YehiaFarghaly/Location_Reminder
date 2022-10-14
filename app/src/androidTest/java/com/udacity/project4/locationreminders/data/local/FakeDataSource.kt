b00d12f334bdce2d9a2f2114eb6185b58e3e9e16
Yehia Mohamed <102627389+YehiaFarghaly@users.noreply.github.com>
2022-10-14 11:39:07 +0200

    Update FakeDataSource.kt

--- app/src/androidTest/java/com/udacity/project4/locationreminders/data/local/FakeDataSource.kt
+++ app/src/androidTest/java/com/udacity/project4/locationreminders/data/local/FakeDataSource.kt
@@ -3,39 +3,48 @@ package com.udacity.project4.locationreminders.data
 import com.google.android.gms.common.api.ResultTransform
 import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.data.dto.Result
-import java.lang.Exception
+
 
 //Use FakeDataSource that acts as a test double to the LocalDataSource
-class FakeDataSource(var tasks:MutableList<ReminderDTO>?= mutableListOf()) : ReminderDataSource {
-
+class FakeDataSource() : ReminderDataSource {
+    // tasks now is not nullable as mentioned in the review
+    var tasks:MutableList<ReminderDTO> = mutableListOf()
     private var shouldReturnError=false
     //    TODO: Create a fake data source to act as a double to the real data source
     override suspend fun getReminders(): Result<List<ReminderDTO>> {
-            if(shouldReturnError)
+        try {
+            if (shouldReturnError)
                 return Result.Error("Error")
-                tasks?.let { return Result.Success(it) }
-        // case of null return an empty list ( as specified in the review)
-        tasks = mutableListOf()
-        return Result.Success(tasks!!)
+            tasks.let { return Result.Success(it) }
+            return Result.Success(tasks)
+        }
+        catch(exception:Exception){
+            return Result.Error(exception.localizedMessage)
+        }
     }
     fun setShouldReturnError(flag:Boolean) {
         shouldReturnError=flag
     }
 
     override suspend fun saveReminder(reminder: ReminderDTO) {
-        tasks?.add(reminder)
+        tasks.add(reminder)
     }
 
     override suspend fun getReminder(id: String): Result<ReminderDTO> {
-   if(shouldReturnError) return Result.Error("Error")
-        for(item in tasks!!) {
-            if(item.id==id) return Result.Success(item)
+        try {
+            if (shouldReturnError) return Result.Error("Error")
+            for (item in tasks) {
+                if (item.id == id) return Result.Success(item)
+            }
+            return Result.Error("No item found")
         }
-        return Result.Error("No item found")
+        catch(exception:Exception){
+            return Result.Error(exception.localizedMessage)
+        }
     }
 
     override suspend fun deleteAllReminders() {
-        tasks?.clear()
+        tasks.clear()
     }
 
 
